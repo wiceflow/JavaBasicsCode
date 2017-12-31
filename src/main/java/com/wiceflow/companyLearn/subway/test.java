24af6cd4f010056439357690f5f160c964a945a5
iceflow <183173916@qq.com>
2017-12-31 17:27:56 +0800

    新增注释，考虑修改

--- src/main/java/com/wiceflow/companyLearn/subway/test.java
+++ src/main/java/com/wiceflow/companyLearn/subway/test.java
@@ -11,6 +11,8 @@ import java.util.*;
 
 /**
  * Created by BF on 2017/12/29.
+ * 等待时间在另外一张excel表中，也可以取出交由程序赋值
+ * 新增线路可以按固定格式写表  也可以手动往数据库插入数据
  */
 public class test {
     public static void main(String[] args) {
@@ -20,13 +22,15 @@ public class test {
         session.beginTransaction();
 
         Query que = session.createQuery("from LineEntity");
-        // 处理成Map形式
+        // 处理成Map形式 key=站点名称 value=站点编号
         List<LineEntity> lineEntitylist = que.list();
+
         Map<String,Integer> map = getLineName(lineEntitylist);
 
         List<WaitingTime> waitingTimeList = getWaitTime(map);
 
-        List<WaitingTime> waitingTimeList2 = getWaitTime2(waitingTimeList);
+        List<WaitingTime> waitingTimeList2 = getWaitTime2(
+                waitingTimeList);
 
         for (WaitingTime w:waitingTimeList2)
         {
@@ -58,6 +62,7 @@ public class test {
             String[] values = value.split("-");
             String startName = values[0];
             String endName = values[1];
+            // 从map中获取站点名称对应的站点编号
             int startCode = m.get(startName);
             int endCode = m.get(endName);
             WaitingTime waitingTime = new WaitingTime();
@@ -70,6 +75,7 @@ public class test {
 
     /**
      * 将实体类转换成 键值对形式
+     * 1号线 - 268
      * @return
      */
     private static Map<String,Integer> getLineName(List<LineEntity> lists){
@@ -79,8 +85,22 @@ public class test {
         }
         return map;
     }
+
+    /**
+     * 设置各个站点等待时间
+     * 这里都是人为设置case,具体的在excel 可以考虑读表
+     * @param w
+     * @return
+     */
     private static List<WaitingTime> getWaitTime2(List<WaitingTime> w){
+        /*
+            WaitingTime waitingTime = new WaitTime();
+         */
         for (int i=0;i<w.size();i++){
+            //TODO 移动到外面去
+            /*
+            waitingTime = w.get(i);
+             */
             WaitingTime waitingTime = w.get(i);
             switch (waitingTime.getEndLine()){
                 case 268:
