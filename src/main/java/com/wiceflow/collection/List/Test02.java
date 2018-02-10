4c744c5103452ef16ca10d5b85706faa445918b8
iceflow <183173916@qq.com>
2018-02-10 23:53:00 +0800

    测试类

---
+++ src/main/java/com/wiceflow/collection/List/Test02.java
@@ -0,0 +1,21 @@
+package com.wiceflow.collection.List;
+
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Created by BF on 2017/11/12.
+ */
+public class Test02 {
+    public static void main(String[] args) {
+        Map a = new HashMap();
+
+        System.out.println(1<<30);
+        char aa = '3';
+        int b = aa + 3;
+        System.out.println(b);
+
+
+    }
+}
