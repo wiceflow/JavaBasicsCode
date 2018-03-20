66585107af3e4b552ac3f3831d078e6b1e166dba
wiceflow <183173916@qq.com>
2018-03-20 14:22:37 +0800

    redis集群笔记

--- src/main/java/com/wiceflow/Test.java
+++ src/main/java/com/wiceflow/Test.java
@@ -6,6 +6,7 @@ import com.wiceflow.companyLearn.subway.TransforMap.GetWaitingTime;
 import com.wiceflow.companyLearn.subway.TransforMap.NewGetWaitingTime;
 import com.wiceflow.excel.util.poi.ImportExcel;
 import com.wiceflow.util.ReadUtil;
+import redis.clients.jedis.Jedis;
 
 import java.io.File;
 import java.sql.SQLException;
@@ -19,48 +20,10 @@ import java.util.Map;
  */
 public class Test {
     public static void main(String[] args) {
-//        GetWaitingTime g = new GetWaitingTime();
-//        int t = g.getWaitingTime(11,1,1);
-//        System.out.println(t);
-//        ReadUtil r = new ReadUtil();
-//        String path = "F:\\票价\\换乘表.csv";
-//        String path2 = "F:\\票价\\line.csv";
-//        List<TransferStation> list = r.getTransferStation(path,path2);
-//        Dao dao = new Dao();
-//        try {
-//            dao.saveTransferStation(list);
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//            System.out.println("失败");
-//        }
+        Jedis jedis = new Jedis("193.112.16.152");
 
-//        GetWaitingTime g = new GetWaitingTime();
-//        System.out.println(g.getWaitingTime(5, 9, 1));
-//
-//        NewGetWaitingTime g2 = new NewGetWaitingTime();
-//        System.out.println(g2.getWaitingTime(5, 9, 180125175248L));
+        System.out.println("success");
 
-
-//        File excelFile = new File("F:\\换乘表.xlsx");
-//        String fileName = excelFile.getName();
-//        String prefix=fileName.substring(fileName.lastIndexOf(".")+1);
-//
-//
-//        System.out.println(fileName);
-//        System.out.println(prefix);
-        String path = "F:\\换乘表.xlsx";
-        ImportExcel importExcel = new ImportExcel();
-        try {
-            List<List<Object>> lists = importExcel.parseExcel(path);
-            for (int i = 0; i < lists.size(); i++) {
-                List<Object> list = lists.get(i);
-                for (int j = 0; j < list.size(); j++) {
-                    System.out.print(list.get(j) + "   ");
-                }
-                System.out.print("\n");
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        System.out.println(jedis.ping());
     }
 }
